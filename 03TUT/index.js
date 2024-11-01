c5ea539bbbcba5946ced350e74ca3ca7e1a9807f
kingjeongkong <wjdqls9223@gmail.com>
2024-11-01 16:57:51 +0900

    03) npm 모듈 설치 및 설정

--- 03TUT/index.js
+++ 03TUT/index.js
@@ -1,1 +1,6 @@
-console.log('hellooo');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+
+console.log(uuid());
