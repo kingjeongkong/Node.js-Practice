7810b89f263c935e160178b8259b83b6c18d9c9e
kingjeongkong <wjdqls9223@gmail.com>
2024-11-05 16:25:26 +0900

    07) CORS middleware 구현

--- 06,07TUT/server.js
+++ 06,07TUT/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const path = require('path');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+// Corss Origin Resource Sharing
+const whitelist = ['https://www.asdf.com', 'http://127.0.0.1:5500', 'localhost:3500'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 app.use(express.urlencoded({ extended: false }));
@@ -61,5 +77,7 @@ app.get('/chain(.html)?', [one, two, three]);
 app.get('/*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
