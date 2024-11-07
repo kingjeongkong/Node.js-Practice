0d9013eb7a66e8ebdc22d34bd742dd2b2e4f9fca
kingjeongkong <wjdqls9223@gmail.com>
2024-11-07 18:25:42 +0900

    10) 아이디 auth api 구현

--- 10TUT/server.js
+++ 10TUT/server.js
@@ -26,6 +26,7 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/employees', require('./routes/api/employees'));
 app.use('/register', require('./routes/api/register'));
+app.use('/auth', require('./routes/api/auth'));
 
 app.get('/*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
