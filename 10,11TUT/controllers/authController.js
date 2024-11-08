37bb019504b26e9e5f8020a65b77d19a4927bdad
kingjeongkong <wjdqls9223@gmail.com>
2024-11-08 19:38:07 +0900

    11) refresh token 로직 구현

--- 10,11TUT/controllers/authController.js
+++ 10,11TUT/controllers/authController.js
@@ -25,7 +25,7 @@ const handleLogin = async (req, res) => {
     const accessToken = jwt.sign(
       { username: foundUser.username },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '30s' }
+      { expiresIn: '5m' }
     );
     const refreshToken = jwt.sign(
       { username: foundUser.username },
--- 10,11TUT/server.js
+++ 10,11TUT/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -20,13 +21,17 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 // built in middleware to serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
-app.use('/register', require('./routes/api/register'));
-app.use('/auth', require('./routes/api/auth'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
 
 app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
